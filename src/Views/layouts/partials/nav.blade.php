f3b30b1d16fcfac0a80d29f9eed9cdc38fdf9025
Chris Gmyr <cmgmyr@gmail.com>
2014-08-26 07:15:22 -0400

    updated header link area with view user helper

--- src/Views/layouts/partials/nav.blade.php
+++ src/Views/layouts/partials/nav.blade.php
@@ -11,12 +11,21 @@
             <a class="navbar-brand" href="{{URL::to('')}}">Song Suggest</a>
         </div>
 
-        @if(Auth::check())
+        @if($currentUser)
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="nav navbar-nav">
                 <li><a href="#">Add Song</a></li>
-                <li class="{{ Route::is('account') ? 'active' : '' }}">{{link_to_route('account', 'Update Account')}}</li>
-                <li>{{link_to_route('logout', 'Logout')}}</li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ $currentUser->first_name}} <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>{{link_to_route('account', 'Update Account')}}</li>
+                        <li class="divider"></li>
+                        <li>{{link_to_route('logout', 'Logout')}}</li>
+                    </ul>
+                </li>
             </ul>
         </div>
         @endif
---
+++ src/views.php
@@ -0,0 +1,3 @@
+<?php
+
+View::share('currentUser', Auth::user());
