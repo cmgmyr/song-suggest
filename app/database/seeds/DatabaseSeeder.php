e6a46b1bfcfc6c2b04de2048f8b28bbc56cddc93
Chris Gmyr <cmgmyr@gmail.com>
2014-09-02 15:05:38 -0400

    updated main seeder file

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,22 +1,67 @@
 <?php
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+    /**
+     * @var
+     */
+    protected $environment;
+
+    /**
+     * @var array
+     */
+    protected $tables = [
+        'songs',
+        'users'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $seeders = [
+        'UsersSeeder'
+    ];
+
+    public function __construct()
+    {
+        $this->environment = App::environment();
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
 
         // start with a blank database
-        DB::table('songs')->truncate();
-        DB::table('users')->truncate();
+        $this->cleanDatabase();
 
         // deploy the seeders!
-		$this->call('UsersSeeder');
-	}
+        foreach ($this->seeders as $seed) {
+            $this->call($seed);
+        }
+    }
+
+    /**
+     * Clean the database for new seeds
+     */
+    private function cleanDatabase()
+    {
+        if ($this->environment != 'testing') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        }
+
+        foreach ($this->tables as $table) {
+            DB::table($table)->truncate();
+        }
+
+        if ($this->environment != 'testing') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        }
+    }
 
 }
