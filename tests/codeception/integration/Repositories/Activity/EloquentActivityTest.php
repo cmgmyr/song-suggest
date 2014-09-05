fb9e9d92cae538ab45a18783e52505ad997dc400
Chris Gmyr <cmgmyr@gmail.com>
2014-09-05 13:26:36 -0400

    added integration test for saving activities

---
+++ tests/codeception/integration/Repositories/Activity/EloquentActivityTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Laracasts\TestDummy\Factory as TestDummy;
+
+class EloquentActivityTest extends \Codeception\TestCase\Test
+{
+
+    /**
+     * @var \IntegrationTester
+     */
+    protected $tester;
+
+    protected $repo;
+
+    protected function _before()
+    {
+        $this->repo = $this->tester->grabService('Ss\Repositories\Activity\ActivityInterface');
+    }
+
+    /** @test */
+    public function save_a_activity()
+    {
+        $activity = TestDummy::create('Ss\Repositories\Activity\Activity', ['message' => 'New Message']);
+
+        $newActivity = 'Another new message';
+
+        $activity->message = $newActivity;
+
+        $repoActivity = $this->repo->save($activity);
+
+        $this->assertEquals($repoActivity->message, $newActivity);
+    }
+
+}
