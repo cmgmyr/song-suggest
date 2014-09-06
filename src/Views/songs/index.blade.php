597ba87095a4e20fc23c361ec28e6bf6987400a7
Chris Gmyr <cmgmyr@gmail.com>
2014-09-06 08:08:41 -0400

    adjusted songs table

--- src/Views/songs/index.blade.php
+++ src/Views/songs/index.blade.php
@@ -2,28 +2,26 @@
 
 @section('content')
 @if(count($songs) > 0)
-<div class="table-responsive clear">
-    <table class="table table-hover table-condensed table-bordered table-striped">
-        <thead>
-        <tr>
-            <td>Artist</td>
-            <td>Title</td>
-            <td>Manage</td>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($songs as $song)
-        <tr>
-            <td>{{$song->artist}}</td>
-            <td>{{$song->title}}</td>
-            <td>
-                @include('songs.index-manage-buttons')
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
+<table class="table table-hover table-condensed table-bordered table-striped song-table">
+    <thead>
+    <tr>
+        <td>Artist</td>
+        <td>Title</td>
+        <td>Manage</td>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($songs as $song)
+    <tr>
+        <td>{{$song->artist}}</td>
+        <td>{{$song->title}}</td>
+        <td class="manage-buttons">
+            @include('songs.index-manage-buttons')
+        </td>
+    </tr>
+    @endforeach
+    </tbody>
+</table>
 @else
 <p>Sorry, there are no records.</p>
 @endif
