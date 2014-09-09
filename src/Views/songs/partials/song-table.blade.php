c01414bca7f3ecc90cf2fae4f2670565b3c05f33
Chris Gmyr <cmgmyr@gmail.com>
2014-09-09 13:32:58 -0400

    Show votes on song index page

--- src/Views/songs/partials/song-table.blade.php
+++ src/Views/songs/partials/song-table.blade.php
@@ -4,6 +4,7 @@
     <tr>
         <td>Artist</td>
         <td>Title</td>
+        <td>Votes</td>
         <td>Manage</td>
     </tr>
     </thead>
@@ -12,6 +13,10 @@
     <tr>
         <td>{{$song->artist}}</td>
         <td>{{$song->title}}</td>
+        <td>
+            <span class="btn btn-success btn-xs">{{$song->positiveVotes()->count()}}</span>
+            <span class="btn btn-danger btn-xs">{{$song->negativeVotes()->count()}}</span>
+        </td>
         <td class="manage-buttons">
             @include('songs.partials.index-manage-buttons')
         </td>
