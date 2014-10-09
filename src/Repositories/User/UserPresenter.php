cb1fb61af776c98bda50727d3b140e4ae09b52f1
Chris Gmyr <cmgmyr@gmail.com>
2014-10-09 08:44:20 -0400

    added user image column and basic handling

--- src/Repositories/User/UserPresenter.php
+++ src/Repositories/User/UserPresenter.php
@@ -3,7 +3,8 @@ namespace Ss\Repositories\User;
 
 use Laracasts\Presenter\Presenter;
 
-class UserPresenter extends Presenter {
+class UserPresenter extends Presenter
+{
 
     /**
      * Gets the URL for the user's avatar image
@@ -12,6 +13,15 @@ class UserPresenter extends Presenter {
      * @return string
      */
     public function avatar($size = 30)
+    {
+        if ($this->image != null) {
+            // @todo: handle uploaded user image
+        } else {
+            return $this->gravatar($size);
+        }
+    }
+
+    protected function gravatar($size)
     {
         $email = md5($this->email);
 
