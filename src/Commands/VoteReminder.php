7bacc1d38a8192544594d88de671457509402d37
Chris Gmyr <cmgmyr@gmail.com>
2014-10-26 12:34:18 -0400

    update formatting

--- src/Commands/VoteReminder.php
+++ src/Commands/VoteReminder.php
@@ -12,24 +12,25 @@ use Ss\Repositories\Song\Song;
 use Ss\Repositories\Song\SongInterface;
 use Ss\Repositories\User\UserInterface;
 
-class VoteReminder extends ScheduledCommand {
+class VoteReminder extends ScheduledCommand
+{
 
     use DispatchableTrait;
     use EventGenerator;
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'song:vote-reminder';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'song:vote-reminder';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Sends an email to users that haven\'t voted for a song';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends an email to users that haven\'t voted for a song';
 
     /**
      * @var SongInterface
@@ -46,9 +47,9 @@ class VoteReminder extends ScheduledCommand {
      *
      * @return \Ss\Commands\VoteReminder
      */
-	public function __construct(UserInterface $user, SongInterface $song)
-	{
-		parent::__construct();
+    public function __construct(UserInterface $user, SongInterface $song)
+    {
+        parent::__construct();
 
         $this->song = $song;
         $this->user = $user;
@@ -60,23 +61,23 @@ class VoteReminder extends ScheduledCommand {
      * @param Scheduler|Schedulable $scheduler
      * @return \Indatus\Dispatcher\Scheduling\Schedulable
      */
-	public function schedule(Schedulable $scheduler)
-	{
+    public function schedule(Schedulable $scheduler)
+    {
         // check/send daily from 9am-6pm, every 15 minutes
-		return $scheduler->hours([9, 10, 11, 12, 13, 14, 15, 16, 17, 18])->minutes([0, 15, 30, 45]);
-	}
+        return $scheduler->hours([9, 10, 11, 12, 13, 14, 15, 16, 17, 18])->minutes([0, 15, 30, 45]);
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
         // find all songs that haven't been reminded
         $songs = $this->song->remindable(Config::get('settings.vote_reminder_days'));
 
-		if ($songs->count() > 0) {
+        if ($songs->count() > 0) {
             foreach ($songs as $song) {
                 // @todo: this doesn't work...$this->execute(UpdateRemindedAtDate::class, ['song' => $song]);
                 $song = Song::updateRemindedAt($song);
@@ -99,6 +100,6 @@ class VoteReminder extends ScheduledCommand {
         }
 
         $this->info('Total Remindable Songs: ' . $songs->count());
-	}
+    }
 
 }
