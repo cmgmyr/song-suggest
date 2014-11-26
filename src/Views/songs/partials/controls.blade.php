12ca407901adb0506317080a105118ad1973fc66
Chris Gmyr <cmgmyr@gmail.com>
2014-11-26 09:25:27 -0500

    added new button for future vote resets

--- src/Views/songs/partials/controls.blade.php
+++ src/Views/songs/partials/controls.blade.php
@@ -2,6 +2,10 @@
     <hr class="hidden-xs hidden-sm">
     <h4>Admin Controls:</h4>
 
+    {{ Form::open(['route' => ['songs.reset', $song->id], 'method' => 'put', 'style' => 'display:inline;', 'data-confirm' => 'Are you sure?', 'class' => 'delete-confirm']) }}
+        <button type="submit" class="btn btn-warning">Reset Votes</button>
+    {{ Form::close() }}
+
     {{ Form::open(['route' => ['songs.destroy', $song->id], 'method' => 'delete', 'style' => 'display:inline;', 'data-confirm' => 'Are you sure?', 'class' => 'delete-confirm']) }}
         <button type="submit" class="btn btn-danger">Delete</button>
     {{ Form::close() }}
