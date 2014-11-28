c9fa025092f7e264c075dd345d798d92395ff956
Chris Gmyr <cmgmyr@gmail.com>
2014-11-28 09:42:59 -0500

    added reset votes command and handler

---
+++ src/Domain/Song/ResetVotesCommand.php
@@ -0,0 +1,18 @@
+<?php
+namespace Ss\Domain\Song;
+
+use Ss\Repositories\Song\Song;
+
+class ResetVotesCommand
+{
+
+    /**
+     * @var Song
+     */
+    public $song;
+
+    function __construct(Song $song)
+    {
+        $this->song = $song;
+    }
+} 
---
+++ src/Domain/Song/ResetVotesCommandHandler.php
@@ -0,0 +1,41 @@
+<?php
+namespace Ss\Domain\Song;
+
+use Laracasts\Commander\CommandHandler;
+use Laracasts\Commander\Events\DispatchableTrait;
+use Ss\Repositories\Category\Category;
+use Ss\Repositories\Song\Song;
+use Ss\Repositories\Song\SongInterface;
+
+class ResetVotesCommandHandler implements CommandHandler
+{
+    use DispatchableTrait;
+
+    /**
+     * @var SongInterface
+     */
+    protected $song;
+
+    function __construct(SongInterface $song)
+    {
+        $this->song = $song;
+    }
+
+    /**
+     * Handle the command
+     *
+     * @param $command
+     * @return mixed
+     */
+    public function handle($command)
+    {
+        $song = Song::updateCategory($command->song, Category::PENDING);
+        $song = Song::resetVotes($song);
+
+        $this->song->save($song);
+
+        $this->dispatchEventsFor($song);
+
+        return $song;
+    }
+}
