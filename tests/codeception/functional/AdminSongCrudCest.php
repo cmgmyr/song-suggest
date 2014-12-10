15463455f9b28168d6db096c9965acba7e1c80d7
Chris Gmyr <cmgmyr@gmail.com>
2014-12-10 16:32:17 -0500

    added test for resetting song votes

---
+++ tests/codeception/functional/AdminSongCrudCest.php
@@ -0,0 +1,23 @@
+<?php
+use \FunctionalTester;
+
+class AdminSongCrudCest
+{
+
+    public function _before(FunctionalTester $I)
+    {
+        $I->signIn(null, null, 'y');
+    }
+
+    // tests
+    public function resetSongVotes(FunctionalTester $I)
+    {
+        $song = $I->haveASong();
+
+        $I->amOnRoute('songs.show', array('id' => $song->id));
+
+        $I->click('Reset Votes');
+
+        $I->seeInSession('flash_notification.message', 'The song votes have been reset, and changed to "Pending".');
+    }
+}
