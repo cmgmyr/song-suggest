e2aa59b194b4906622a6d14b6ac4bd783cc30bb2
Chris Gmyr <cmgmyr@gmail.com>
2015-09-23 20:09:37 -0400

    update tests

--- tests/codeception/_support/FunctionalHelper.php
+++ tests/codeception/_support/FunctionalHelper.php
@@ -4,7 +4,6 @@ namespace Codeception\Module;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-use FunctionalTester;
 use Laracasts\TestDummy\Factory as TestDummy;
 
 class FunctionalHelper extends \Codeception\Module
